0fd1308b5e25fd9a2b1ae059c1f3d6c9b11499ec
web-padawan <skamandr@yandex.ru>
2015-12-22 08:42:11 +0200

    [mod] banner: logo styles

--- themes/evroteh/templates/views-view-field--slider--block--uri.tpl.php
+++ themes/evroteh/templates/views-view-field--slider--block--uri.tpl.php
@@ -1,4 +1,8 @@
 <?php
   $output = image_style_url('slider_wide', $output);
 ?>
-  <div class="slider" style="background-image: url('<?php print $output; ?>');">&nbsp;</div>
+  <div class="slider" style="background-image: url('<?php print $output; ?>');">
+    <div class="slider__container">
+      <div class="slider__logo">&nbsp;</div>
+    </div>
+  </div>
