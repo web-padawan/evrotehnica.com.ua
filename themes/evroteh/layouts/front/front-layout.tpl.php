4a9d740ccd7a842dcfdcb7a6d8578c60cba33f50
web-padawan <skamandr@yandex.ru>
2015-12-22 09:18:00 +0200

    [new] clients block

--- themes/evroteh/layouts/front/front-layout.tpl.php
+++ themes/evroteh/layouts/front/front-layout.tpl.php
@@ -19,9 +19,27 @@
     <?php print views_embed_view('slider', 'block'); ?>
   </header>
 
+  <div class="clients">
+    <div class="clients__container">
+      <h2 class="clients__title">Наши клиенты</h2>
+      <ul class="clients__list">
+        <li class="clients__item"><a class="clients__link clients__link--mts" href="#">&nbsp;</a></li>
+        <li class="clients__item"><a class="clients__link clients__link--vega" href="#">&nbsp;</a></li>
+        <li class="clients__item"><a class="clients__link clients__link--reikartz" href="#">&nbsp;</a></li>
+        <li class="clients__item"><a class="clients__link clients__link--atb" href="#">&nbsp;</a></li>
+        <li class="clients__item"><a class="clients__link clients__link--uz" href="#">&nbsp;</a></li>
+        <li class="clients__item"><a class="clients__link clients__link--eva" href="#">&nbsp;</a></li>
+        <li class="clients__item"><a class="clients__link clients__link--prostor" href="#">&nbsp;</a></li>
+        <li class="clients__item"><a class="clients__link clients__link--brusnichka" href="#">&nbsp;</a></li>
+        <li class="clients__item"><a class="clients__link clients__link--abk" href="#">&nbsp;</a></li>
+        <li class="clients__item"><a class="clients__link clients__link--garvis" href="#">&nbsp;</a></li>
+      </ul>
+    </div>
+  </div>
+
   <div class="objects">
     <div class="objects__container">
-      <div class="objects__title">Объекты</div>
+      <h2 class="objects__title">Объекты</h2>
       <div class="objects__content">
         <?php print views_embed_view('objects', 'block'); ?>
       </div>
